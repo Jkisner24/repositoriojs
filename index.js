bd1c0a8244f75fa47dcc93b1a6c9ef98b85ecd3e
Julian Kisner <julikisner@gmail.com>
2022-05-19 21:30:54 -0300

    segunda entrega

--- index.js
+++ index.js
@@ -1,275 +1,91 @@
-class Producto{
+const contenedor = document.getElementById("productos");
+const tablaCarrito = document.getElementById("tablaCarrito");
+const carrito = []; 
 
-    constructor(tipo, turno, precio){
-        this.tipo = tipo;
-        this.turno = turno;
-        this.precio = precio;
-    }
+const PRODUCTOS = [
+    {
+        id: 1,
+        nombre: "Funcional",
+        precio: 5000,
+        stock: 5,
+        imagen: "../img/GimNova17-2-6.jpg"
+    },
+    {
+        id: 2,
+        nombre: "Crossfit",
+        precio: 4500,
+        stock: 10,
+        imagen: "../img/GimNova17-2-21.jpg"
+    },
+    {
+        id: 3,
+        nombre: "Semi-Privado",
+        precio: 4800,
+        stock: 8,
+        imagen: "../img/GimNova17-2-36.jpg"
+    }];
 
-    obtenerDatos(){
+const getCard = (item) => {
+    return (
+        `
+        <div class="card" style="width: 33.3%">
+            <img src="${item.imagen}" class="card-img-top" alt="${item.nombre}">
+            <div class="card-body">
+                <h5 class="card-title">${item.nombre}</h5>
+                <p class="card-text">$${item.precio}</p>
+                <p class="card-text">Stock: ${item.stock}</p>
+                <button onclick=agregarCarrito(${item.id}) class="btn ${item.stock ? 'btn-primary' : 'btn-secondary'}" ${!item.stock ? 'disabled' : '' } >Agregar clase al carrito</button>
+            </div>
+        </div>
+    `);
+};
 
-        return `Tipo: ${this.tipo}.\nTurno: ${this.turno}.\nPrecio: ${this.precio}.`;
-    }
+const getRow = (item) => {
+    return(
+        `
+    <tr>
+        <th scope="row">${item.id}</th>
+        <td>${item.nombre}</td>
+        <td>${item.cantidad}</td>
+        <td>$${item.precio * item.cantidad} ($${item.precio})</td>
+        <td><img style="width:20px" src="${item.imagen}" alt="imagen"></td>
+    </tr>
+        `
+    )
 }
 
-class Carrito{
+const cargarProductos = (datos, nodo, esTabla) => {
+    let acumulador = "";
+    datos.forEach((el) => {
+        acumulador += esTabla ? getRow(el) : getCard(el);
+    })
+    nodo.innerHTML = acumulador;
+};
 
-    constructor(){
-
-        this.productosSeleccionados = [];/* array vacio para ir sumando productos */
-    }
-
-    agregarProducto(nuevoProducto){
-
-        this.productosSeleccionados.push(nuevoProducto);/* push para ir sumando productos */
-    }
-
-    eliminarProducto(productoAEliminar){
-
-        let index = this.productosSeleccionados.indexOf(productoAEliminar); /* indexOff para buscar objeto */
-
-        this.productosSeleccionados.splice(index, 1); /* splice para eliminar producto determinado */
-    }
-
-    obtenerProductosSeleccionados(){
-
-        let auxProductosSeleccionados = [];
-
-        for(let i = 0; i < this.productosSeleccionados.length; i++){
-
-            auxProductosSeleccionados.push( `Producto: ${i + 1}.\n` + this.productosSeleccionados[i].obtenerDatos() + "\n");
-
-        }
-
-        auxProductosSeleccionados.push(`\nIngrese 0 si no quiere eliminar ningun producto.\n`);
-
-        return auxProductosSeleccionados.join("\n");
-    }
-}
-
-class Catalogo{
-
-    constructor(producto, stock){
-        this.producto = producto;
-        this.stock = stock; 
-    }
-
-    agregarStock(cantidad){
-
-        this.stock = this.stock + cantidad;
-    }
-
-    reducirStock(cantidad){
-
-        this.stock = this.stock - cantidad;
-    }
-
-    obtenerDatos(){
-
-        return this.producto.obtenerDatos() + `\nStock: ${this.stock}.`; 
-
-    }
-}
-
-class Supermercado{
-
-    constructor(){
-
-        this.catalogo = [];
-
-        this.carrito = new Carrito();
-
-    }
-
-    agregarCatalogoProducto(catalogo){
-        this.catalogo.push(catalogo);
-    }
-
-    obtenerCatalogo(){
-
-        let auxCatalogo = [];
-
-        for(let i = 0; i < this.catalogo.length; i++){
-
-            if(this.catalogo[i].stock > 0)
-                auxCatalogo.push( `Producto: ${i + 1}.\n` + this.catalogo[i].obtenerDatos() + "\n");
-
-        }
-
-        auxCatalogo.push(`\nIngrese 0 si no quiere agregar ningun producto.\n`);
-
-        return auxCatalogo.join("\n");
-    }
-
-    obtenerIndiceCatalogo(catalogo){
-        return this.catalogo.find(catalogo);
-    }
-
-}
-
-/* ----------------------------------------------------------------------- */
-/* base de datos */
-function cargaPreviaDatos(){
-    const productoUno = new Producto("Crossfit", "Mañana", 3200);
-    const productoDos = new Producto("Funcional", "Tarde", 3500);
-    const productoTres = new Producto("Semi Privado", "Noche", 3300);
-
-    const supermercado = new Supermercado();
-
-    supermercado.agregarCatalogoProducto(new Catalogo(productoUno, 20));
-    supermercado.agregarCatalogoProducto(new Catalogo(productoDos, 25));
-    supermercado.agregarCatalogoProducto(new Catalogo(productoTres, 18));
-
-    return supermercado;
-}
-
-/* Mensaje inicial */
-const mensajeBienvenida = "-- Bienvenido al sistema de turnos --\n" +
-                          "1 - Agregar clases al carrito.\n" +
-                          "2 - Eliminar clases del carrito.\n" +
-                          "3 - Ver clases agregadas al carrito.\n" +
-                          "\n0 - Salir del programa.\n";
-
-/* Validaciones */
-const validarRango = (valor) =>{ return (minimo, maximo) => minimo <= valor && maximo >= valor }
-
-const validarOpcionIngresada= (minimo, maximo, mensaje) => {
-
-    let opcion;
-    let auxFuncion;
-
-    do{
-
-        opcion = parseInt(prompt(mensaje));
-
-        auxFuncion = validarRango(opcion);
-
-    } while(!auxFuncion(minimo, maximo));
-
-    return opcion;
-}
-
-/* Menu inicial */
-const supermercado = cargaPreviaDatos();
-
-/* Funciones */
-function agregarProductoCarrito() {
-
-    let cantidad = 0;
-
-    for(const aux of supermercado.catalogo){
-        if(aux.stock > 0)
-         cantidad++;
-    }
-
-    let producto = validarOpcionIngresada(0, cantidad, supermercado.obtenerCatalogo());
+const agregarCarrito = (id) => {
+    const seleccion = PRODUCTOS.find(item => item.id === id);
+    const busqueda = carrito.findIndex(el => el.id === id);
     
-    if (!Number.isNaN(producto)){
-        if(producto != 0) {
-            producto--;
-            let productoSeleccionado = supermercado.catalogo[producto].producto;
-            supermercado.carrito.agregarProducto(productoSeleccionado);
-            supermercado.catalogo[producto].reducirStock(1);
-        }
-    }
-}
-
-function eliminarProductoCarrito(){
-
-    let producto = validarOpcionIngresada(0, supermercado.carrito.productosSeleccionados.length, supermercado.carrito.obtenerProductosSeleccionados());
-
-    if (!Number.isNaN(producto)){
-        if(producto != 0) {
-      
-            producto--;
-
-            let productoSeleccionado = supermercado.carrito.productosSeleccionados[producto];
-
-            supermercado.catalogo.forEach((element)=>{
-                if (element.producto == productoSeleccionado){
-                    element.agregarStock(1);
-                }
-            });
-
-            supermercado.carrito.eliminarProducto(productoSeleccionado);
-        }
-    }
-}
-
-function finalizarCompra(){
-
-    let producto = validarOpcionIngresada(0, supermercado.carrito.productosSeleccionados.length, supermercado.carrito.obtenerProductosSeleccionados());
-
-    if (!Number.isNaN(producto)){
-
-        producto--;      
-            
-        let producto = alert(supermercado.catalogo[producto].producto);
-
-    }
-}
-
-function ejecutarOpcion(opcion){
-
-    if(opcion == 0){
-
-        return true;
-
-    } else if (opcion == 1){
-
-        agregarProductoCarrito();
-        
-    } else if(opcion == 2) {
-
-        eliminarProductoCarrito();
-
-    }else if (opcion == 3){
-        
-        finalizarCompra();
-    
+    if (busqueda === -1) {
+        carrito.push({
+            id: seleccion.id,
+            nombre: seleccion.nombre,
+            precio: seleccion.precio,
+            cantidad: 1,
+            imagen: seleccion.imagen,
+        })
     } else {
-        
-        return false;    
+        carrito[busqueda].cantidad = carrito[busqueda].cantidad + 1
     }
+    
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    cargarProductos(carrito, tablaCarrito, true);
 }
 
-let terminarPrograma = false;
+cargarProductos(PRODUCTOS, contenedor, false);
 
-while(!terminarPrograma){
-
-    let opcion = validarOpcionIngresada(0, 3, mensajeBienvenida);
-
-    terminarPrograma = ejecutarOpcion(opcion);
+function inicializarCarrito(){
+    carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    cargarProductos(carrito, tablaCarrito, true);
 }
-
-
-/* Interactuando con HTML */
-
-let horarios = document.getElementById("horarios");
-
-horarios.innerHTML = "Horarios sujetos a disponibilidad. Por cambios presionar aqui";
-
-
-/* Agregando nodos
-
-let nombre = document.createElement("p");
-nombre.innerHTML = "<p>Horarios sujetos a disponibilidad</p>"
-document.body.append(nombre); */
-
-
-/* Trabajando con eventos */
-let evento = "mousedown";
-let boton = document.getElementById("cambioHorario");
-boton.addEventListener(evento, () => {
-
-    console.log("click con" + evento);
-})
-
-/* let formulario = document.getElementById("");
-
-
-formulario.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log("Formulario enviado")
-}); */
 
